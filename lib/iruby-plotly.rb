11dc2567fd2f669d847a442240e2437d33d7ca10
kojix2 <2xijok@gmail.com>
2019-05-24 16:19:54 +0900

    autoindent

--- lib/iruby-plotly.rb
+++ lib/iruby-plotly.rb
@@ -10,19 +10,19 @@ module IRuby
   end
 
   def self.plot(data, options = {})
-        if data.respond_to?(:keys)
-            options = options.merge(data)
-        if data.include?(:xy) then
-          data = data.clone
-          data[:x] = data[:xy].map(&:first)
-          data[:y] = data[:xy].map(&:last)
-          data.delete(:xy)
-        end
-        data = [data]
-      elsif not data.first.respond_to?(:keys)
-        data = [{y:data, x:(1..data.size).to_a}.merge(options)]
+    if data.respond_to?(:keys)
+      options = options.merge(data)
+      if data.include?(:xy) then
+        data = data.clone
+        data[:x] = data[:xy].map(&:first)
+        data[:y] = data[:xy].map(&:last)
+        data.delete(:xy)
       end
-        IRuby.convert({data: data, layout: {height: DEFAULT_PLOT_HEIGHT}.merge(options)}, mime: "application/vnd.plotly.v1+json")
+      data = [data]
+    elsif not data.first.respond_to?(:keys)
+      data = [{y:data, x:(1..data.size).to_a}.merge(options)]
+    end
+    IRuby.convert({data: data, layout: {height: DEFAULT_PLOT_HEIGHT}.merge(options)}, mime: "application/vnd.plotly.v1+json")
   end
 
   def self.plotly(data, layout = {})
